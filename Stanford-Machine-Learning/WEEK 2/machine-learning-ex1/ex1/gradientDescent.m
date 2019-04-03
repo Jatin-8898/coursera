70dbc2e7d48a2c50f663953bb943bab963c472f0
Jatin Varlyani <varlyanijatin88@gmail.com>
2019-04-03 23:06:15 +0530

    Added Gradient Descent Cide

--- Stanford-Machine-Learning/WEEK 2/machine-learning-ex1/ex1/gradientDescent.m
+++ Stanford-Machine-Learning/WEEK 2/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,9 +16,14 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
+  
+    % Applying the gradient descent formula
+    
+    delta = X * theta - y;
+    
+    theta = theta - alpha * (1 / m) * X' *delta;
 
-
-
+ 
 
 
 
